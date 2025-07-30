31c8f0d4f30841a8ad7e755f1a8aa3da04467d68
omkar-2005 <omkarmodhave2@gmail.com>
2025-07-30 17:34:14 +0530

    Add files via upload

---
+++ create_post.php
@@ -0,0 +1,5 @@
+<form method="POST" action="store_post.php">
+    <input type="text" name="title" placeholder="Enter Title" required><br>
+    <textarea name="content" placeholder="Write content here" required></textarea><br>
+    <button type="submit">Publish</button>
+</form>
